2afe8a2469b55db381f258b034010db107dac94b
dedmasai <114438951+dedmasai@users.noreply.github.com>
2023-11-13 00:02:51 +0300

    d

--- myauth/urls.py
+++ myauth/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.views import LoginView
-from django.urls import path
+from django.urls import path, resolve
 from .views import login_view
 
 app_name='myauth'
 
 urlpatterns = [
-  path('login/', LoginView.as_view(template_name="myauth/login.html"), name='login' ),
+  path('login/', login_view, name='login' ),
 ]
--- requestdataapp/views.py
+++ requestdataapp/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def process_get_view(request:HttpRequest)->HttpResponse:
+    context={
+    }
+    return render(request, "requestdataapp/request_querry_params.html", context=context)
